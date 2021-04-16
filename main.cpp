233bed92baab5df601c70cf10870aa582c1aeb47
Gabriel Reis <>
2021-04-16 20:20:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int tamanhoPalavra;
+  
+  cout << "Bem vindo ao programa de escrita do celular\n" << endl;
+  cout << "Para escrever a palavra, insira quantos dígitos tem (máx 50): \n";
+  cin >> tamanhoPalavra;
+  cout << tamanhoPalavra;
+
+}
